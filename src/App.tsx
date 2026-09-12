4e84e640584d37e1d0e9e34fdfdef4ef0d22aa06
Yousuf <149528624+abdullah-yousuf-26@users.noreply.github.com>
2026-09-12 16:20:07 +0600

    allfiles

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+    <h2 className="text-5xl text-red-500">Dev Stack </h2> 
+    <button className="btn btn-warning">Warning</button>
+
+    </>
+  )
+}
+
+export default App
